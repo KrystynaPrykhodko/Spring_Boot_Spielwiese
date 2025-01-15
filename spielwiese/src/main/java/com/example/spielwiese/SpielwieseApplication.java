083ecf2a26c016acd1f4be52c7d80b253a3f6106
Krystyna Prykhodko <krystyna.prykhodko@gmail.com>
2025-01-15 17:26:43 +0100

    Initial Spring Boot project added

---
+++ spielwiese/src/main/java/com/example/spielwiese/SpielwieseApplication.java
@@ -0,0 +1,13 @@
+package com.example.spielwiese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpielwieseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpielwieseApplication.class, args);
+	}
+
+}
